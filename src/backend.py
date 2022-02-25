ca82e4de9c2a935ca00885c302f154f2d0aa0168
jamesversch17 <james.r.verschleiser.22@dartmouth.edu>
2022-02-25 11:44:04 -0500

    fixed import

--- src/backend.py
+++ src/backend.py
@@ -2,7 +2,7 @@
 # Within each of these routes, there should not be any computation occuring
 # Instead, we handle all data parsing and handling in the controllers file
 
-import src.controllers
+import src.controllers as controllers
 
 # Credit to https://pythonbasics.org/flask-tutorial-hello-world/ prints hello world
 from werkzeug.wrappers import Response
